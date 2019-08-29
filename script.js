e0a3ad97e81f8cc9e86592431e5d3ebf2e52bbfd
Lee <gwforumlakers@gmail.com>
2019-08-29 08:56:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -20,65 +20,41 @@ angular.module('ionicApp', ['ionic', 'ngResource'])
 
   $urlRouterProvider.otherwise("/event/home");
  })
-
  .run(function($rootScope, $http) {
-  var ii = 0;
-  var jj = 0;
+  let ii=0, jj=0, newRS=0, hh=0, tS01={};
+ 
   // global variables  $rootScope
-  $rootScope.cusRecipe = { "crSolv": "sBB", "crArr": [ {"cas": "c00001", "conc": 101}, {"cas": "c00002", "conc": 201}, {"cas": "c00003", "conc": 301}, {"cas": "c00004", "conc": 401}, {"cas": "c00005", "conc": 499}  ] };
+  var tS00 = {"Team":"--", "Scores":[0,0,0,0,0,0,0,0,0], "Putts":[0,0,0,0,0,0,0,0,0] };
+  $rootScope.Teams = [];
 
- /*
-recipe from screen:  solv + casArr;
-for each cas from recipe, lookup rSolv[key].casArr[key2].cas
-Convert concentration to standard UOM
-return lowest shelf life;  check concentration range;  choose solvent
-check analyte master list;  inorganic / organic
-*/
-    $http.get('https://api.airtable.com/v0/app0hohtq4b1nM0Kb/Solvents/recQH2Vz0tLC8Qfbq?api_key=key66fQg5IghIIQmb') 
-      .success(function (jsonData) {
-        $rootScope.rSolv = JSON.parse(jsonData.fields.Analytes);
+  // alert($rootScope.rawS.length);
+  $http.get('https://api.airtable.com/v0/app0hohtq4b1nM0Kb/Skins?api_key=key66fQg5IghIIQmb')
+    .success(function (jData) {
+      $rootScope.rawS = JSON.parse(JSON.stringify(jData.records));
+
+      for (ii = 0; ii < $rootScope.rawS.length; ii++) {
+        newRS = -1;
+        for (jj = 0; jj < $rootScope.Teams.length; jj++) {
+          if ($rootScope.Teams[jj].Team === $rootScope.rawS[ii].fields.Team) {
+            newRS = jj; hh = $rootScope.rawS[ii].fields.Hole - 1;
+            $rootScope.Teams[jj].Scores[hh] = $rootScope.rawS[ii].fields.Score;
+            $rootScope.Teams[jj].Putts[hh] = $rootScope.rawS[ii].fields.Putts;
+          }
+        }
+        if (newRS < 0) {
+          tS01 = JSON.parse(JSON.stringify(tS00));
+          tS01.Team = $rootScope.rawS[ii].fields.Team;
+          hh = $rootScope.rawS[ii].fields.Hole - 1;
+          tS01.Scores[hh] = $rootScope.rawS[ii].fields.Score;   
+          tS01.Putts[hh] = $rootScope.rawS[ii].fields.Putts;   
+          $rootScope.Teams.push(JSON.parse(JSON.stringify(tS01)));
+        }
+      }
     });
-
-  $rootScope.xxTimes = function(nn) {
-   var ii = 0;
-   var aa = [];
-   for (ii = 0; ii < nn; ii++) {
-    if (arguments.length > 1) {
-     aa[ii] = arguments[1];
-    } else {
-     aa[ii] = ii;
-    }
-   }
-   return aa;
-  };
 
 })
 .controller('MainCtrl', function($rootScope, $scope, $http) {
-
- $scope.valRecipe = function() {
-   // alert('valRecipe');
- for (var key in $rootScope.rSolv) {
-
-  if ($rootScope.cusRecipe.crSolv === $rootScope.rSolv[key].solv) { 
-       alert($rootScope.rSolv[key].solv);
-     
-     for (var key2 in $rootScope.rSolv[key].casArr) {
-       if ($rootScope.cusRecipe.crArr[0].cas === $rootScope.rSolv[key].casArr[key2].cas) { 
-          alert($rootScope.rSolv[key].casArr[0].cas);
-          alert($rootScope.rSolv[key].casArr[0].maxconc);
-          // alert(rSolv[key].casArr[0].shlife);
-          if ( $rootScope.cusRecipe.crArr[0].conc <= $rootScope.rSolv[key].casArr[0].maxconc) {
-            alert($rootScope.rSolv[key].casArr[0].maxconc);
-            }
-          else { 
-            alert("conc fallout");
-          }
-          alert($rootScope.rSolv[key].casArr[0].shlife);
-       }
-     }
-  }
-}
-
- };
- 
+  
 });
+
+// More code goes here ...
