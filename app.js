7713708bd4b5b93bbb16ede8f7d76f7596854b69
Lee <gwforumlakers@gmail.com>
2019-09-17 23:05:17 -0700

    Update app.js

--- app.js
+++ app.js
@@ -36,50 +36,74 @@ angular.module('gscAppH', ['ionic', 'ngResource'])
   
   $urlRouterProvider.otherwise("/event/home");
 })
-.run(function($rootScope, $http) {
-  let ii=0, jj=0, newRS=0, hh=0, tS01={};
-  var tS00 = {"Team":"--", "Scores":[0,0,0,0,0,0,0,0,0], "Putts":[0,0,0,0,0,0,0,0,0], "tsF9": 0, "tpF9": 0, "tsB9": 0, "tpB9": 0, "tsR18": 0, "tpR18": 0, "tPrize": 0 };
-  $rootScope.Teams = [];
 
-  // alert($rootScope.rawS.length);
+.run(function($rootScope, $http) {
+  let ii=0, jj=0, newRS=true, tt=0, hh=0, 
+    tS00 = {name:"--", scores:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            putts:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            tsF9: 0, tpF9: 0, tsB9: 0, tpB9: 0, tsR18: 0, tpR18: 0,
+            tPrize: 0, th: 0, ts: 0, tw: 0,
+            s: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            h: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            w: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            u1:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            u2: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    grp00={ name: "__", isShown: false };
+
+  $rootScope.accGroup = [];  // grp00={ name: "__", isShown: false };
+  for (hh = 0; hh < 18; hh++) {        
+    $rootScope.accGroup.push(JSON.parse(JSON.stringify(grp00)));
+  };
+
+  $rootScope.vMatch = {when: "When", loc: "Where", pp: 0, cp: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], ch: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], pz4: [25,10,5,2,0,-1,-2,-4,-6,-8] };
+  $rootScope.byTeam = [];
+
   $http.get('https://api.airtable.com/v0/app0hohtq4b1nM0Kb/Skins?api_key=key66fQg5IghIIQmb')
     .success(function (jData) {
       $rootScope.rawS = JSON.parse(JSON.stringify(jData.records));
 
       for (ii = 0; ii < $rootScope.rawS.length; ii++) {
-        newRS = -1;
-        for (jj = 0; jj < $rootScope.Teams.length; jj++) {
-          if ($rootScope.Teams[jj].Team === $rootScope.rawS[ii].fields.Team) {
-            newRS = jj; hh = $rootScope.rawS[ii].fields.Hole - 1;
-            $rootScope.Teams[jj].Scores[hh] = $rootScope.rawS[ii].fields.Score;
-            $rootScope.Teams[jj].Putts[hh] = $rootScope.rawS[ii].fields.Putts;
+        newRS = true;
+        hh = $rootScope.rawS[ii].fields.Hole - 1;
+        for (jj = 0; jj < $rootScope.byTeam.length; jj++) {
+          if ($rootScope.byTeam[jj].name === $rootScope.rawS[ii].fields.Team) {
+            newRS = false; 
+            $rootScope.byTeam[jj].scores[hh] = $rootScope.rawS[ii].fields.Score;
+            $rootScope.byTeam[jj].putts[hh] = $rootScope.rawS[ii].fields.Putts;
           }
         }
-        if (newRS < 0) {
-          tS01 = JSON.parse(JSON.stringify(tS00));
-          tS01.Team = $rootScope.rawS[ii].fields.Team;
-          hh = $rootScope.rawS[ii].fields.Hole - 1;
-          tS01.Scores[hh] = $rootScope.rawS[ii].fields.Score;   
-          tS01.Putts[hh] = $rootScope.rawS[ii].fields.Putts;   
-          $rootScope.Teams.push(JSON.parse(JSON.stringify(tS01)));
+        if (newRS) {
+          $rootScope.byTeam.push(JSON.parse(JSON.stringify(tS00)));
+          jj = $rootScope.byTeam.length -1;
+          $rootScope.byTeam[jj].name = $rootScope.rawS[ii].fields.Team;
+          $rootScope.byTeam[jj].scores[hh] = $rootScope.rawS[ii].fields.Score;
+          $rootScope.byTeam[jj].putts[hh] = $rootScope.rawS[ii].fields.Putts;   
         }
       }
 
-      for (jj = 0; jj < $rootScope.Teams.length; jj++) {
-      for (hh = 0; hh < $rootScope.Teams[jj].Scores.length; hh++) {
-        if (hh < 9) {
-          $rootScope.Teams[jj].tsF9 += $rootScope.Teams[jj].Scores[hh];
-          $rootScope.Teams[jj].tpF9 += $rootScope.Teams[jj].Putts[hh];
-        } else {
-          $rootScope.Teams[jj].tsB9 += $rootScope.Teams[jj].Scores[hh];
-          $rootScope.Teams[jj].tpB9 += $rootScope.Teams[jj].Putts[hh];
+      for (jj = 0; jj < $rootScope.byTeam.length; jj++) {
+        for (hh = 0; hh < 9; hh++) {        
+            $rootScope.byTeam[jj].tsF9 += $rootScope.byTeam[jj].scores[hh];
+            $rootScope.byTeam[jj].tpF9 += $rootScope.byTeam[jj].putts[hh];
         }
+        for (hh = 9; hh < 18; hh++) {        
+            $rootScope.byTeam[jj].tsB9 += $rootScope.byTeam[jj].scores[hh];
+            $rootScope.byTeam[jj].tpB9 += $rootScope.byTeam[jj].putts[hh];
+        }
+        $rootScope.byTeam[jj].tsR18 = $rootScope.byTeam[jj].tsF9 + $rootScope.byTeam[jj].tsB9;
+        $rootScope.byTeam[jj].tpR18 = $rootScope.byTeam[jj].tpF9 + $rootScope.byTeam[jj].tpB9;
       }
-      $rootScope.Teams[jj].tsR18 = $rootScope.Teams[jj].tsF9 + $rootScope.Teams[jj].tsB9;
-      $rootScope.Teams[jj].tpR18 = $rootScope.Teams[jj].tpF9 + $rootScope.Teams[jj].tpB9;
-      }
+
+    /* 
+    */
     });
+
 })
+
 .controller('MainCtrl', function($rootScope, $scope, $http) {
-  
+
+  $scope.toggleGroup = function(group) {
+    group.isShown = !group.isShown;
+  };
+
 });
